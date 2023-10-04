7063c4463a00eb9d35472d40edd5421612f5dd0a
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2023-10-03 21:44:04 -0500

    Create Objects.java

---
+++ Class Notes/Objects.java
@@ -0,0 +1,75 @@
+public class Objects
+{
+  public static void main(String[] args)
+  {
+    /*
+    So far in this class, we've really been focused on variables and methods.
+    In particular, we've combined variables (data) and methods (functions, behaviors, actions)
+    to build programs.
+
+    We are going to move from "procedural decomposition", the idea of
+    breaking big tasks into small ones done by methods, to object-oriented programming.
+    */
+
+    /*
+    Objects
+
+    Objects are the combination of data (variables) and behavior (methods).
+    They are both. 
+    
+    Other examples of entities made of multiple parts would be 
+    things like how the government is made of three branches, or how many religions
+    think that "man" is made of multiple parts like body, soul, spirit, etc.
+
+    An example of an Object is a String. Strings have data (the text of the String)
+    and also behaviors (methods) such as charAt() or length().
+
+    For now, let's not worry about how to create our own Objects, but instead just
+    think about why we use them and how to use them.
+    */
+
+    /*
+    Motivation
+
+    For now, we've been working 
+
+    Objects:
+    Reduce complexity of programs
+    Bundles data together with relevant methods
+    Are always built for a specific task or goal
+    Are re-useable
+    "Abstract" away unnecessary details (things we don't want to worry about) 
+    */
+
+    /*
+    Data (Instance Variables)
+    
+    */
+
+    /*
+    Behavior (Instance Methods) 
+    
+    */
+
+    /*
+    Making an Object (Constructor)
+    
+    */
+
+    /*
+    Implicit Parameters 
+
+    */
+
+    /*
+    A BankAccount Object
+    
+    */
+
+    /*
+    A Dog Object
+    
+    */
+  }
+  
+}
