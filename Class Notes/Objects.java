f14105266f5640a3f6f4acb2e058f93aa68167b6
Matthew-Bounds-Teacher <122050578+Matthew-Bounds-Teacher@users.noreply.github.com>
2023-10-04 08:59:00 -0500

    Update Objects.java

--- Class Notes/Objects.java
+++ Class Notes/Objects.java
@@ -29,7 +29,7 @@ public class Objects
     So, it's fair to not just think of Strings as chains of characters,
     but also as including methods that make these characters useful, like .indexOf().
 
-    For now, let's not worry about how to create our own Objects, but instead just
+    For now, let's not worry about how to make new kinds of Objects, but instead just
     think about why we use them and how to use them.
     */
 
@@ -37,21 +37,42 @@ public class Objects
     Motivation
     As we build larger programs, we might get into situations
     where we can't really manage all these different methods and variables.
-    Objects organize related data and behaviors into a single entity.
+    Objects organize related data and behaviors into a single encapsulated entity.
     
     Also, objects are useful because they:
-    
+
+    Represent real-world concepts in computer science
     Reduce complexity of programs (tons of variables -> one object)
     Are always built for a specific task or goal
-    Are re-useable
+    Are re-usable
     "Abstract" away unnecessary details (implementations we don't need to know about)
+    */
+
+    /*
+    Making an Object (Constructor)
+
+    We mentioned that we aren't going to define our own Objects for a while, 
+    but we can still "make" them in our programs just like how we 
+    made new variables. 
+
+    To "construct" an object, we use the following syntax:
+    Object_type name = new Object_type(parameters);
+    Often parameters are the data of our objects.
+
+    For example, we've used syntax like:
+    Scanner in = new Scanner(System.in);
+
+    In this case, Scanner is the type of our Object (A Scanner object)
+    and "in" is the name of this object.
+    System.in is a parameter (it's not relevant why it's a parameter)
+    
     */
 
     /*
     Data (Instance Variables)
     
     Objects contain relevant data. This data is in the form of variables.
-    Another term for Objects are instances, so we refer these variables
+    Another term for Objects is instances, so we refer to these variables
     as instance variables. 
 
     We can use instance variables in the same way that we use other variables.
@@ -68,37 +89,6 @@ public class Objects
 
     In a sense, we are saying:
     "Hey, object_name, go ahead and do method_name()". 
-    */
-
-    /*
-    Making an Object (Constructor)
-
-    We mentioned that we aren't going to define our own Objects for a while, 
-    but we can still "make" them in our programs just like how we 
-    made new variables. 
-
-    To "construct" an object, we use the following syntax:
-    Object_type name = new Object_type(parameters);
-
-    For example, we've used syntax like:
-    Scanner in = new Scanner(System.in);
-
-    In this case, Scanner is the type of our Object (A Scanner object)
-    and "in" is the name of this object.
-    System.in is a parameter (it's not relevant why it's a parameter)
-    
-    */
-
-    /*
-    Implicit Parameters 
-    
-    
-    */
-
-    /*
-    A BankAccount Object
-    
-    
     */
   }
   
