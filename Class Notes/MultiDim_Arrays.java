7cf9e2b94f1015e8709ef9e706dda29f4397a05c
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2023-11-16 18:50:46 +0000

    git commit

--- Class Notes/MultiDim_Arrays.java
+++ Class Notes/MultiDim_Arrays.java
@@ -4,41 +4,41 @@ public class MultiDim_Arrays
 {
     public static void main(String[] args)
     {
-        /*
-         * Often a 1-Dimensional list of information isn't enough we can create 
-         * arrays with multiple dimensions. This can help us represent more complex
-         * real world objects such as a grid or a cube. This is achieved in java by
-         * creating an array of arrays.
-         * 
-         * The syntax is simiar to 1-Dimensional array.
-         * 
-         * <element_type>[][] <array_name> = new <element_type>[<row_length>][<column_length>]; 
-         * int[][] array = new int[3][4]; // this creates an integer array with 3 rows and 4 columns
-         *                                   0   1   2   3
-         *                                 ┌────────┬────────┬────────┬────────┐
-         *                               0 │ [0][0] │ [0][1] │ [0][2] │ [0][3] │
-         *                                 ├────────┼────────┼────────┼────────┤
-         *                               1 │ [1][0] │ [1][1] │ [1][2] │ [1][3] │
-         *                                 ├────────┼────────┼────────┼────────┤
-         *                               2 │ [2][0] │ [2][1] │ [2][2] │ [2][3] │
-         *                                 └────────┴────────┴────────┴────────┘
-         * 
-         * As well we can construct our arrays with initial values much the same way as before
-         * <element_type>[][] <array_name> = {{<value>, <value>, ..., <value>},
-         *                                    {<value>, <value>, ..., <value>}};
-         * int[][] array = {{1, 2, 3},
-         *                  {4, 5, 6},
-         *                  {7, 8, 9}};
-         *  
-         */
+        //  *
+        //  * Often a 1-Dimensional list of information isn't enough we can create 
+        //  * arrays with multiple dimensions. This can help us represent more complex
+        //  * real world objects such as a grid or a cube. This is achieved in java by
+        //  * creating an array of arrays.
+        //  * 
+        //  * The syntax is simiar to 1-Dimensional array.
+        //  * 
+        //  * <element_type>[][] <array_name> = new <element_type>[<row_length>][<column_length>]; 
+        //  * int[][] array = new int[3][4]; // this creates an integer array with 3 rows and 4 columns
+        //  *                                   0   1   2   3
+        //  *                                 ┌────────┬────────┬────────┬────────┐
+        //  *                               0 │ [0][0] │ [0][1] │ [0][2] │ [0][3] │
+        //  *                                 ├────────┼────────┼────────┼────────┤
+        //  *                               1 │ [1][0] │ [1][1] │ [1][2] │ [1][3] │
+        //  *                                 ├────────┼────────┼────────┼────────┤
+        //  *                               2 │ [2][0] │ [2][1] │ [2][2] │ [2][3] │
+        //  *                                 └────────┴────────┴────────┴────────┘
+        //  * 
+        //  * As well we can construct our arrays with initial values much the same way as before
+        //  * <element_type>[][] <array_name> = {{<value>, <value>, ..., <value>},
+        //  *                                    {<value>, <value>, ..., <value>}};
+        //  * int[][] array = {{1, 2, 3},
+        //  *                  {4, 5, 6},
+        //  *                  {7, 8, 9}};
+        //  *  
+        //  *
 
-        //*
         int[][] array = {{1, 2, 3},
                          {4, 5, 6},
-                         {7, 8, 9}
+                         {7, 8, 9},
                          {10, 11, 12}};
-        System.out.println("Rows: array.length);
-        System.out.println("Columns": array[i].length);
+
+        System.out.println("Rows:" + array.length);
+        System.out.println("Columns:" + array[0].length);
         
         for(int i = 0; i < array.length; i++)
         {
@@ -48,7 +48,6 @@ public class MultiDim_Arrays
             }
             System.out.println();
         }
-        //*/
 
     }
 
