67da70aa4e635d683cbe13f6140f05d06b568234
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2023-11-16 12:46:31 -0600

    Update MultiDim_Arrays.java

--- Class Notes/MultiDim_Arrays.java
+++ Class Notes/MultiDim_Arrays.java
@@ -15,13 +15,13 @@ public class MultiDim_Arrays
          * <element_type>[][] <array_name> = new <element_type>[<row_length>][<column_length>]; 
          * int[][] array = new int[3][4]; // this creates an integer array with 3 rows and 4 columns
          *                                   0   1   2   3
-         *                                 ┌───┬───┬───┬───┐
-         *                               0 │ 0 │ 0 │ 0 │ 0 │
-         *                                 ├───┼───┼───┼───┤
-         *                               1 │ 0 │ 0 │ 0 │ 0 │
-         *                                 ├───┼───┼───┼───┤
-         *                               2 │ 0 │ 0 │ 0 │ 0 │
-         *                                 └───┴───┴───┴───┘
+         *                                 ┌────────┬────────┬────────┬────────┐
+         *                               0 │ [0][0] │ [0][1] │ [0][2] │ [0][3] │
+         *                                 ├────────┼────────┼────────┼────────┤
+         *                               1 │ [1][0] │ [1][1] │ [1][2] │ [1][3] │
+         *                                 ├────────┼────────┼────────┼────────┤
+         *                               2 │ [2][0] │ [2][1] │ [2][2] │ [2][3] │
+         *                                 └────────┴────────┴────────┴────────┘
          * 
          * As well we can construct our arrays with initial values much the same way as before
          * <element_type>[][] <array_name> = {{<value>, <value>, ..., <value>},
@@ -33,24 +33,13 @@ public class MultiDim_Arrays
          */
 
         //*
-        Scanner input = new Scanner(System.in);
-        String[][] arr = new String[4][4];
-
-        for(int i = 0; i < arr.length; i++)
-        {
-            for(int j = 0; j < arr[i].length; j++)
-            {
-                System.out.print("Please enter a word: ");
-                arr[i][j] = input.next();
-            }
-        }
-        //*/
-
-        //*
         int[][] array = {{1, 2, 3},
                          {4, 5, 6},
-                         {7, 8, 9}};
-
+                         {7, 8, 9}
+                         {10, 11, 12}};
+        System.out.println("Rows: array.length);
+        System.out.println("Columns": array[i].length);
+        
         for(int i = 0; i < array.length; i++)
         {
             for(int j = 0; j < array[i].length; j++)
