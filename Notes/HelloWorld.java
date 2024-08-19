c8b88fbcdb1b5c6eb3d7365a4b113939908f6027
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2024-08-19 15:23:51 +0000

    YAY!

--- Notes/HelloWorld.java
+++ Notes/HelloWorld.java
@@ -11,6 +11,7 @@ Notes Outline:
 
 /*
 0) Review of PB&J Activity
+
 - What instructions were ambiguous?
 - What went well?
 - How do you get good at explaining your thoughts to others? 
@@ -19,21 +20,41 @@ Notes Outline:
 /*
 1) What is programming? 
 
-Definitions: 
+Multiple Definitions:
+- Process of writing computer programs
+  - Giving a computer a list of instructions to execute
+    - If you've followed a recipe or Google Maps before, you were the computer!  
+- Teaching computers how to do something (writing algorithms)
+- More than just coding
 */
 
 /*
 2) Thinking algorithmically
+
+Algorithms
+Definitions:
+- step-by-step description of solving a problem
+- How computers interpret things (literally, and in a step-by-step manner)
+
+How and why to think algorithmically?
+- Pragmatic since it produces a result (an algorithm) that a computer can understand
+- Makes hard problems easier
+- Kind of like mathematical proofs
+  - We know the beginning and ending of the problem, we just need to figure out
+    the steps in the middle 
 */
 
 /*
 3) "Hello, World!"
+
+Go ahead and open the "Lab 0" assignment on the school website. 
+We're going to write our first program together. 
 */
 
-*/
 public class HelloWorld {
     public static void main(String[] args)
     {
+      
     }
   }
 
