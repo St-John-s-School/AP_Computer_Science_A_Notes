e08d6bbf2d46b947e48598e601ed3b5b31bbffbb
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2024-08-19 14:32:39 +0000

    Writing some notes!

---
+++ Notes/HelloWorld.java
@@ -0,0 +1,51 @@
+/*
+This is how we're going to take/read/do notes in the class.
+
+Notes Outline:
+0) Review of PB & J Activity
+1) What is programming? 
+2) Thinking Algorithmically
+3) "Hello, World!"
+4) git commit 
+*/
+
+/*
+0) Review of PB&J Activity
+- What instructions were ambiguous?
+- What went well?
+- How do you get good at explaining your thoughts to others? 
+*/
+
+/*
+1) What is programming? 
+
+Definitions: 
+*/
+
+/*
+2) Thinking algorithmically
+*/
+
+/*
+3) "Hello, World!"
+*/
+
+*/
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+    }
+  }
+
+/*
+4) git commit
+
+When you're done coding (or at a checkpoint), it's time to commit your changes
+to a repository. 
+
+Algorithm:
+
+0) Go to the "Source Control" tab 
+1) Press "Commit"
+
+*/
