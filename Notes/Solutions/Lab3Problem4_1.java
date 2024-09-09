34389ea7a18091e8a02cb9066196366c122ee5f6
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2024-09-09 13:17:30 +0000

    yuh

---
+++ Notes/Solutions/Lab3Problem4_1.java
@@ -0,0 +1,10 @@
+public class Lab3Problem4_1
+{
+    public static void main(String[] args)
+    {
+        String s = "test string please ignore";
+        int a = 5;
+        String sub = s.substring(a, a + 1);
+        System.out.println(sub);  
+    }
+}
