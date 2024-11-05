8efa465e8bc7108c1ebd4a557ee61a24b012a870
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2024-11-05 00:24:18 +0000

    hehe

---
+++ Notes/MoreLoops.java
@@ -0,0 +1,22 @@
+public class MoreLoops
+{
+    /* 
+    Outline:
+    
+    0) Some Solutions
+    1) Nested Loops
+    2) Do-While Loops
+    3) Practice
+
+    */
+
+    public static void main(String[] args)
+    {
+        // int i = 0;
+        // do {
+        // System.out.println(i);
+        // i++;
+        // }
+        // while (i < 5);
+    }
+} 
