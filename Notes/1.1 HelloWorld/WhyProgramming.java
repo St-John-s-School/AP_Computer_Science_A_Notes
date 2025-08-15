0ba2de4400ef3b9dc3b252edbb94bbb92e255dd8
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2025-08-15 02:25:26 +0000

    Set up for 25-26

---
+++ Notes/1.1 HelloWorld/WhyProgramming.java
@@ -0,0 +1,50 @@
+/*
+Welcome to AP CS A! Before we write any computer programs,
+we'll think about why programming is useful.
+
+A minor note on logistics:
+Feel free to take notes wherever. You can use Google Docs, 
+paper, whatever. These notes will always be available online.
+In the future, they'll be less verbose. 
+
+1) What is programming? 
+
+Most of y'all completed a PB&J activity where you thought
+about giving specific, step-by-step instructions to someone else.
+Programming is that process, just the "someone else" is a computer,
+and your instructions are in a language like Java, not English.
+
+Literally, programming is the process of writing computer programs.
+To do this, a programmer gives a list of instructions to a computer
+to execute. If you've ever followed a recipe or used Google Maps 
+to get to your friend's house, YOU were the computer, since you
+were the one following the instructions. More generally, programming
+isn't just the process of typing these commands out. We need to know
+how the computer will interpret those commands, in what order to
+write commands, and how to fix our code if the commands don't work.
+
+2) Thinking algorithmically
+
+In order to communicate with computers, we need to know how computers
+think. An algorithm is a precise, step-by-step list of instructions
+for solving a problem or accomplishing a task. You probably learned
+the importance of thinking algorithmically during the PB&J activity.
+If you weren't precise, then your instructions weren't followed well!
+
+Thinking algorithmically will eventually let us break down complex
+problems into a set of smaller steps. This will be valuable, even if
+you never write another computer program after this class. It'll be
+a way to think about accomplishing things even without a computer.
+In this class, it'll help us write complex computer programs by 
+combining more simple commands. We might not think of making 
+sandwiches as complex, but if you had to explain how to make one
+to someone, wouldn't a list of instructions be an acceptable way? 
+*/
+
+public class WhyProgramming
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
