36486f922dcb087eb7882b0a0d4f6b284bddeac8
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2025-08-18 14:10:00 -0500

    Update b_HelloWorld.java

--- Notes/1.1 HelloWorld/b_HelloWorld.java
+++ Notes/1.1 HelloWorld/b_HelloWorld.java
@@ -1,6 +1,15 @@
 /*
 Your file name needs to be the same as whatever comes after
 public class ... 
+
+To print something, put it inside quotes ("") inside of a print command.
+"print" is the first command (method) we will learn in Java.
+In Java, print commands look like this:
+System.out.println("");
+
+Notice that the command ends in a semicolon.
+The semicolon tells your computer that the line is over
+Basically every line of code in Java ends in a semicolon.
 */
 
 public class b_HelloWorld
