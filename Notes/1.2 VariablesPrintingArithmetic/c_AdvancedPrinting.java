6233eab11f62af97bf70664df0f60d012709d185
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2025-08-18 19:21:10 -0500

    Update c_AdvancedPrinting.java

--- Notes/1.2 VariablesPrintingArithmetic/c_AdvancedPrinting.java
+++ Notes/1.2 VariablesPrintingArithmetic/c_AdvancedPrinting.java
@@ -9,5 +9,20 @@ public class c_AdvancedPrinting
 
         int age = 27;
         System.out.println("I am " + age + " years old");
+
+        /*
+        Remember, computers are very, very literal.
+        Variables only change value if we tell the computer to change their value.
+
+        See the commented code below. 
+        What will happen?
+        */
+
+        int a = 4;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Original sum:" + sum);
+        a = 6
+        System.out.println("New sum:" + sum);
     }
 }
