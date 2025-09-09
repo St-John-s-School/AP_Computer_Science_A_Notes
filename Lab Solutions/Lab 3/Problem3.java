f6882b0afc86fb09ef350cd2e456e666a9a8177f
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2025-09-09 17:44:23 +0000

    updated through lab 3

---
+++ Lab Solutions/Lab 3/Problem3.java
@@ -0,0 +1,14 @@
+
+/*
+Credit to Cooper Polyanksy
+*/
+
+public class Problem3{
+  public static void main(String[] args)
+  { 
+   String a = "banana";
+   int index = 4;
+   String b = a.substring(index, index + 1);
+   System.out.println(b);
+  }
+}
