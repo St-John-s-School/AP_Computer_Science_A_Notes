6a1693f57928ef1b921aa950552bb05fe5e512a4
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2025-09-09 17:44:43 +0000

    formatting

--- Lab Solutions/Lab 3/Problem1.java
+++ Lab Solutions/Lab 3/Problem1.java
@@ -5,7 +5,6 @@ Credit to Denzel Briggs!
 public class Problem1{
   public static void main(String[] args)
   {
-    //Write a program that prints out the number of characters in a String, and then prints out the last character of that String. 
     String test = "abcdef";
     int testlength = test.length();
     System.out.println(testlength);
