f7a983f1bb8c90fcfc509b7642be797509c901c9
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2026-01-21 16:43:37 -0600

    Add exception handling to main method

--- Notes/4.3 Text Files/d_Practice.java
+++ Notes/4.3 Text Files/d_Practice.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class d_Practice
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
     /*
     Working with a partner on a whiteboard, write some programs that use ArrayLists
