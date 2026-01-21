58cdf307d23fba87ae6ccbb220d0db9384e10b19
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2026-01-21 16:43:26 -0600

    Modify main method to handle FileNotFoundException
    Updated main method to throw FileNotFoundException.

--- Notes/4.3 Text Files/b_Scanner.java
+++ Notes/4.3 Text Files/b_Scanner.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class b_Scanner
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
     /*
     It turns out that we can read text files in Java with Scanner
