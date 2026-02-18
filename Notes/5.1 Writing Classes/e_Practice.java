66c15ac68e23ff753d333e0b29ecdb2f0184092b
egao-sjs <134416738+egao-sjs@users.noreply.github.com>
2026-02-18 03:08:13 +0000

    asdf

--- Notes/5.1 Writing Classes/e_Practice.java
+++ Notes/5.1 Writing Classes/e_Practice.java
@@ -7,13 +7,25 @@ public class enhanced_Practice
     /*
     The problems in this unit will usually involve writing your own Class. 
 
-    Problem 0 (to be completed as a class):
+    Problem 0: (to be completed as a class):
 
     Write a class called Rectangle that represents rectangles in Java.
 
     Q: What variables should we have?
 
     Q: What methods should we have? 
+
+    Problem 1: (partner, whiteboard, yadda yadda)
+
+    Write the TrafficLight class.
+
+    A TrafficLight object will have one variable (color)
+
+    There will be a constructor, and two other methods
+
+    -void advance(). This will advance the traffic light to the next color.
+    -boolean isSafeToGo().
+     This will return whether it's safe for cars to cross the traffic light.
 
     */
         
